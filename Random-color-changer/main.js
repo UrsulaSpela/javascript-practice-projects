bc4c0a584c0c372ca83d4db3ddf37e835bbda906
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-09-19 11:28:02 +0200

    Add files via upload

--- Random-color-changer/main.js
+++ Random-color-changer/main.js
@@ -1,2 +1,13 @@
 let title = document.querySelector("#color-name")
-const chagerBtn = document.querySelector("#changer-btn")
+const changerBtn = document.querySelector("#changer-btn")
+const colors = ["#BF267A", "#F2B705", "#D98E04", "#F29544", "#F28241"]
+
+changerBtn.addEventListener("click", function(){
+    let i = getRandomNumber()
+    document.body.style.backgroundColor = colors[i]
+    title.textContent = colors[i]
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
