d804c4260f6e95b0d9ce0067f22f1c8a04893bd5
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-09-23 13:39:27 +0200

    Add files via upload

--- Rock-paper-scissors/script.js
+++ Rock-paper-scissors/script.js
@@ -3,6 +3,28 @@ const btnPaper = document.getElementById("btn-paper")
 const btnScissors = document.getElementById("btn-scissors")
 let compChoices = ["rock", "paper", "scissors"]
 
-function playGame(){
-    let compChoice = Math.floor(Math.random)
-}
+// function computerSelection(){
+//     let randomIndex = Math.floor(Math.random() * compChoices.length)
+//     let compChoice = document.getElementById("comp-choice")
+//     compChoice.textContent = ", computer played " + compChoices[randomIndex] +". "
+// }
+// computerSelection()
+// console.log(computerSelection())
+
+btnRock.addEventListener("click", function(){
+    let playersChoice = document.getElementById("players-choice")
+    playersChoice.textContent = "You played rock"
+    
+    let randomIndex = Math.floor(Math.random() * compChoices.length)
+    let compChoice = document.getElementById("comp-choice")
+    compChoice.textContent = ", computer played " + compChoices[randomIndex] +". "
+
+    let result = document.getElementById("winner")
+    if(compChoices[randomIndex] === compChoices[1]){
+        result.textContent = "Computer wins!"
+    } else if(compChoices[randomIndex] === compChoices[2]){
+        result.textContent = "You win!"
+    } else{
+        result.textContent = "It's a tie!"
+    }
+})
