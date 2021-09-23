40fff048e8401ec43ee6afe649eb5f34f476f0e3
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-09-23 12:39:59 +0200

    Add files via upload

--- Rock-paper-scissors/script.js
+++ Rock-paper-scissors/script.js
@@ -1,1 +1,8 @@
-const btnPlay = document.querySelector("#btn-play")
+const btnRock = document.getElementById("btn-rock")
+const btnPaper = document.getElementById("btn-paper")
+const btnScissors = document.getElementById("btn-scissors")
+let compChoices = ["rock", "paper", "scissors"]
+
+function playGame(){
+    let compChoice = Math.floor(Math.random)
+}
