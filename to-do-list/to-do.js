a16e7a22cec7dbdf7433e28ba4eb34fc9da1db51
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-10-09 20:31:26 +0200

    Add files via upload

---
+++ to-do-list/to-do.js
@@ -0,0 +1,8 @@
+const addBtn = document.querySelector("#add-btn")
+let userInput = document.querySelector("input").value
+
+
+
+addBtn.addEventListener("click", function(){
+    console.log(userInput)
+})
