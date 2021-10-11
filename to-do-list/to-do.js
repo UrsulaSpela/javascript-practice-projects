0d0e1acb0c90125302e645c7e205d97ca2a40a85
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-10-11 17:27:29 +0200

    Add files via upload

--- to-do-list/to-do.js
+++ to-do-list/to-do.js
@@ -1,8 +1,12 @@
 const addBtn = document.querySelector("#add-btn")
 let userInput = document.querySelector("input").value
+let userTodos = document.querySelector("#user-todos")
 
 
 
 addBtn.addEventListener("click", function(){
+    let newToDo = document.createElement("p")
+    userTodos.appendChild(newToDo)
+    newToDo.textContent = userInput
     console.log(userInput)
 })
