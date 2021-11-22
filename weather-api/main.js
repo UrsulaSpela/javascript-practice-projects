2874f6674cbd89de59a7211b393c5e971bfafda1
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-11-22 11:21:06 +0100

    Add files via upload

---
+++ weather-api/main.js
@@ -0,0 +1,6 @@
+let todaysDate = document.querySelector("#date");
+let temperature = document.querySelector("#temperature");
+let condition = document.querySelector("#condition");
+let location = document.querySelector("#location");
+let api =
+  "http://api.weatherapi.com/v1/current.json?key=3de5e5ec1a02466ea99101801212211&q=London&aqi=no";
